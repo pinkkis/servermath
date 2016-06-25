c0ce2d0f351e14fd30c06d7b5930ebce761604a2
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2016-06-25 20:58:16 +0100

    Initial Commit with Three sources

---
+++ README.md
@@ -0,0 +1,3 @@
+# Server Math
+-----
+Rewriting some Three.js math classes to be used server side in node.js projects
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Box2 = 	require('./src/Box2');
+const Box3 = 	require('./src/Box3');
+const Euler = 	require('./src/Euler');
+const Line3 = 	require('./src/Line3');
+const _Math = 	require('./src/Math');
+const Matrix3 = require('./src/Matrix3');
+const Matrix4 = require('./src/Matrix4');
+const Plane = 	require('./src/Plane');
+const Ray = 	require('./src/Ray');
+const Sphere = 	require('./src/Sphere');
+const Spline = 	require('./src/Spline');
+const Vector2 = require('./src/Vector2');
+const Vector3 = require('./src/Vector3');
+const Vector4 = require('./src/Vector4');
+
+const SM = {
+	Vector3: Vector3
+};
+
+module.exports = SM;
