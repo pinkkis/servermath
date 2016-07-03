0bd39ae58076d1c02ea6d5e29cb155182df2d24d
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2016-07-03 21:27:07 +0300

    renamed src folder

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 
-const Box2 = 	require('./src/Box2');
-const Box3 = 	require('./src/Box3');
-const Euler = 	require('./src/Euler');
-const Line3 = 	require('./src/Line3');
-const Smath = 	require('./src/Smath');
-const Matrix3 = require('./src/Matrix3');
-const Matrix4 = require('./src/Matrix4');
-const Plane = 	require('./src/Plane');
-const Ray = 	require('./src/Ray');
-const Sphere = 	require('./src/Sphere');
-const Vector2 = require('./src/Vector2');
-const Vector3 = require('./src/Vector3');
-const Vector4 = require('./src/Vector4');
+const Box2 = 		require('./lib/Box2');
+const Box3 = 		require('./lib/Box3');
+const Euler = 		require('./lib/Euler');
+const Line3 = 		require('./lib/Line3');
+const Smath = 		require('./lib/Smath');
+const Matrix3 = 	require('./lib/Matrix3');
+const Matrix4 = 	require('./lib/Matrix4');
+const Plane = 		require('./lib/Plane');
+const Quaternion = 	require('./lib/Quaternion');
+const Ray = 		require('./lib/Ray');
+const Sphere = 		require('./lib/Sphere');
+const Vector2 = 	require('./lib/Vector2');
+const Vector3 = 	require('./lib/Vector3');
+const Vector4 = 	require('./lib/Vector4');
 
 module.exports = {
 	Box2: Box2,
